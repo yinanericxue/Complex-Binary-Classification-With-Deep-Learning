96163bca628cb782b8c4f5d5a0ec2d3ad2464588
yinanericxue <102645083+yinanericxue@users.noreply.github.com>
2022-09-03 11:35:40 -0700

    Update BinaryClassificationHardCode.py

--- BinaryClassificationHardCode.py
+++ BinaryClassificationHardCode.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset
 
 X, Y = load_planar_dataset()
-sample_number = X.shape[1]
-
+sampleSize= X.shape[1]
 learning_rate = 0.1
 
 # Initiate the parameters: (2 + 1) x 4 + (4 + 1) = 17
@@ -14,20 +13,17 @@ W2 = np.random.randn(Y.shape[0], 4) * 0.01
 b2 = np.zeros(shape=(Y.shape[0], 1))
 
 # process of calculation and storage of intermediate variables
+# W1 = 4 x 2, W2 = 1 x 4, b1 = 4 x 1, b2 = 1 x 1
 def forward_propagation(X, W1, W2, b1, b2):
-    Z1 = np.dot(W1, X) + b1
-    A1 = np.tanh(Z1)
-    Z2 = np.dot(W2, A1) + b2
-    A2 = sigmoid(Z2)
-    parameters = {"Z1": Z1,"A1": A1,"Z2": Z2,"A2": A2}
-    return A2, parameters
+    Z1 = np.dot(W1, X) + b1 # Z1 = 4 x 400
+    A1 = np.tanh(Z1) # A1 = 4 x 400
+    Z2 = np.dot(W2, A1) + b2 # Z2 = 1 x 400
+    A2 = sigmoid(Z2) # A2 = 1 x 400
+    return A2, {"Z1": Z1,"A1": A1,"Z2": Z2,"A2": A2}
 
 # cross-entropy is used in this loss function
 def compute_cost(A2, Y, m):
-    cost_sum = np.multiply(np.log(A2), Y) + np.multiply((1 - Y), np.log(1 - A2))
-    cost = - np.sum(cost_sum) / m
-    cost = np.squeeze(cost)
-    return cost
+    return np.squeeze(- np.sum(np.multiply(np.log(A2), Y) + np.multiply((1 - Y), np.log(1 - A2))) / m)
 
 # fine-tuning the weights based on the error rate
 def back_propagation(W1, b1, W2, b2, cache, m):
@@ -48,8 +44,8 @@ def back_propagation(W1, b1, W2, b2, cache, m):
 # 100,000 interations performed
 for i in range(0, 100000):
     A2, parameters = forward_propagation(X, W1, W2, b1, b2)
-    cost = compute_cost(A2, Y, sample_number)
-    W1, W2, b1, b2 = back_propagation(W1, b1, W2, b2, parameters, sample_number)
+    cost = compute_cost(A2, Y, sampleSize)
+    W1, W2, b1, b2 = back_propagation(W1, b1, W2, b2, parameters, sampleSize)
     if cost and i % 10000 == 0:
         print("Cost after iteration % i: % f" % (i, cost))
 
